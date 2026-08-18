87a3cbe84b989cfc491680f47ca809c48fd191c9
geczyakos <geczyakos@gmail.com>
2026-08-18 10:07:12 +0200

    feat: add webhook timeout handling

---
+++ src/timeoutHandling.js
@@ -0,0 +1,6 @@
+export function withTimeout(promise, ms = 5000) {
+  return Promise.race([
+    promise,
+    new Promise((_, reject) => setTimeout(() => reject(new Error('Webhook delivery timed out')), ms)),
+  ]);
+}
